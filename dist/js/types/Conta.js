b8b13f117190c924947faa9d235a71b2fd479ad4
rennebarduco <143908565+rennebarduco@users.noreply.github.com>
2024-01-30 11:00:10 -0300

    Bytebank
    Aplicação Bytebank com programacao orientada a objeto e formatacao no typescript

---
+++ dist/js/types/Conta.js
@@ -0,0 +1,76 @@
+import { TipoTransacao } from "./TipoTransacao.js";
+export class Conta {
+    nome;
+    saldo = JSON.parse(localStorage.getItem("saldo")) || 0;
+    transacoes = JSON.parse(localStorage.getItem("transacoes"), (key, value) => {
+        if (key === "data") {
+            return new Date(value);
+        }
+        return value;
+    }) || [];
+    constructor(nome) {
+        this.nome = nome;
+    }
+    getTitular() {
+        return this.nome;
+    }
+    getGrupoTransacoes() {
+        const grupoTransacoes = [];
+        const listaTransacoes = structuredClone(this.transacoes);
+        const transacoesOrdenadas = listaTransacoes.sort((t1, t2) => t2.data.getTime() - t1.data.getTime());
+        let labelAtualGrupoTransacao = "";
+        for (let transacao of transacoesOrdenadas) {
+            let labelGrupoTransacao = transacao.data.toLocaleDateString("pt-br", { month: "long", year: "numeric" });
+            if (labelAtualGrupoTransacao != labelGrupoTransacao) {
+                labelAtualGrupoTransacao = labelGrupoTransacao;
+                grupoTransacoes.push({
+                    label: labelGrupoTransacao,
+                    transacoes: []
+                });
+            }
+            grupoTransacoes.at(-1).transacoes.push(transacao);
+        }
+        return grupoTransacoes;
+    }
+    getSaldo() {
+        return this.saldo;
+    }
+    getDataAcesso() {
+        return new Date();
+    }
+    registrarTransacao(novaTransacao) {
+        if (novaTransacao.tipoTransacao == TipoTransacao.DEPOSITO) {
+            this.depositar(novaTransacao.valor);
+        }
+        else if (novaTransacao.tipoTransacao == TipoTransacao.TRANSFERENCIA || novaTransacao.tipoTransacao == TipoTransacao.PAGAMENTO_BOLETO) {
+            this.debitar(novaTransacao.valor);
+            novaTransacao.valor *= -1;
+        }
+        else {
+            throw Error("Tipo de Transação é inválido!");
+        }
+        this.transacoes.push(novaTransacao);
+        console.log(this.getGrupoTransacoes());
+        localStorage.setItem("transacoes", JSON.stringify(this.transacoes));
+    }
+    debitar(valor) {
+        if (valor <= 0) {
+            throw new Error("O valor a ser debitado deve ser maior que zero!");
+        }
+        if (valor > this.saldo) {
+            throw new Error("Saldo insuficiente!");
+        }
+        this.saldo -= valor;
+        localStorage.setItem("saldo", this.saldo.toString());
+    }
+    depositar(valor) {
+        if (valor <= 0) {
+            throw new Error("O valor a ser depositado deve ser maior que zero!");
+        }
+        this.saldo += valor;
+        localStorage.setItem("saldo", this.saldo.toString());
+    }
+}
+const conta = new Conta("Joana da Silva Oliveira");
+console.log(conta.getTitular());
+export default conta;
---
+++ src/types/FormatoData.ts
@@ -0,0 +1,5 @@
+export enum FormatoData {
+    PADRAO = "DD/MM/AAAA",
+    DIA_SEMANA_MES_ANO = "DIA_SEMANA, DD/MM/AAAA",
+    DIA_MES = "DD/MM"
+}
